253b79ac8873d3769cdc4f32247c9dd6eb564dea
vyriyang <vyriyang@gmail.com>
2019-09-11 01:20:20 -0500

    imageLink

--- imageLink/imageLink.js
+++ imageLink/imageLink.js
@@ -1,33 +1,23 @@
-
-
 var img;
 
 function preload(){
-    img=loadImage("img/pikapikapika.jpg");
+    img=loadImage("https://github.com/creativeCodingART2210Fall2019Section2/creativeCodingGitHub/raw/master/imageLink/img/pikapikapika.jpg");
     
 }
 
 function setup(){
-    
-
-    createCanvas(windowWidth,windowHeight);
+        createCanvas(windowWidth,windowHeight);
   
-    
-
 }
 
 
 function draw(){
   image(img,windowWidth/2-345,windowHeight/2-194);
   
-
 }
 
 
 function windowResized(){
-
     resizeCanvas(windowWidth,windowHeight); 
-
-    
 
 }
--- projectDemo/projectSubmittingDemo/sketch.js
+++ projectDemo/projectSubmittingDemo/sketch.js
@@ -1,25 +1,17 @@
 function setup(){
-    
     createCanvas(windowWidth,windowHeight);
-    
-
 }
-
 
 function draw(){
     mouseChangingBackground()
-
 }
 
-function mouseChangingBackground(){
-    
+function mouseChangingBackground(){   
     let x1 = map(mouseX, 0, width, 0, 135,true);
     let x2 = map(mouseY, 0, width, 0, 255,true);
     let x3 = map(mouseX, 0, width, 255, 0,true);
     background(x3,x2,x1);
-
 }
-
 
 function windowResized(){
     resizeCanvas(windowWidth,windowHeight)
