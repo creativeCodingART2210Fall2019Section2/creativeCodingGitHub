e24a58c007a828776654c5629d029bc15424bc48
vyriyang <vyriyang@gmail.com>
2019-10-24 17:02:41 -0500

    Update sketch.js

--- projectDemo/projectDemo/randomColourArray/sketch.js
+++ projectDemo/projectDemo/randomColourArray/sketch.js
@@ -24,10 +24,17 @@ function setup() {
 
 function draw() {
 
+    frameRate(30);
+    let cutDown=map(frameCount,0,450,16,1);
+
+    if(frameCount <= 450){
+
     background('#d3d3d3');
     fill(0);
     textSize(12);
+    textAlign(LEFT);
     text("Score: " + score, 10, 20);
+    text("Time: " + int(cutDown), 90, 20);
     
     fill(index1);   
     noStroke();
@@ -41,6 +48,35 @@ function draw() {
     
     }
 
+    if(frameCount>450){
+        background('#d3d3d3');
+        fill(0);
+        textSize(25);
+        textAlign(CENTER);
+        text("SCORE: " + score, windowWidth/2, windowHeight/2);
+        fill(255);
+        rect(windowWidth/2-70, windowHeight/2+30,140,40);
+        fill(0);
+        textSize(25);
+        textAlign(CENTER);
+        text("RESTART",windowWidth/2, windowHeight/2+60)
+
+        let dRw = dist(mouseX, windowHeight/2+30, windowWidth/2-70, windowHeight/2+30);
+        let dRh = dist(windowWidth/2-70, mouseY, windowWidth/2-70, windowHeight/2+30);
+
+        if(dRw<140 && dRh<40){
+            fill('#a9a9a9');
+            rect(windowWidth/2-70, windowHeight/2+30,140,40);
+            fill(255);
+            textSize(25);
+            textAlign(CENTER);
+            text("RESTART",windowWidth/2, windowHeight/2+60)
+        }
+
+
+    }
+
+}
 
 function  mouseClicked(){
     let d = dist(mouseX, mouseY, x, y);
@@ -61,6 +97,23 @@ function  mouseClicked(){
     index2=int(random(0,3));
     index1=c[int(random(0, 3))];
     index0=c[int(random(0, 3))];
+
+    if(frameCount>450){
+        let dRw = dist(mouseX, windowHeight/2+30, windowWidth/2-70, windowHeight/2+30);
+        let dRh = dist(windowWidth/2-70, mouseY, windowWidth/2-70, windowHeight/2+30);
+
+        if(dRw<140 && dRh<40){
+            frameCount=0;
+            background('#d3d3d3');
+            x = random(width);
+            y = random(70,height);
+            index0=c[int(random(0, 3))];
+            index1=c[int(random(0, 3))];
+            index2=0;
+        }
+    
+
+    }
        
 }   
 
