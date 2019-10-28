8102d78f3f97d09f93cb2fd6ba42135d47a3347b
vyriyang <vyriyang@gmail.com>
2019-10-28 12:30:57 -0500

    Update sketch.js

--- projectDemo/projectDemo/json/sketch.js
+++ projectDemo/projectDemo/json/sketch.js
@@ -1,17 +1,22 @@
 var flowers;
 
+/*
 function preload(){
   flowers = loadJSON("flower.json");
 
 }
-
+*/
 function setup(){
   createCanvas(400,400);
+  flowers={
+    name: "sunflower",
+    col:color(200,200,0)
+  }
   
 }
 
 function draw(){
   background(0);
-  fill(flowers.r,flowers.g,flowers.b);
+  fill(flowers.col);
   Text(flowers.name,10,50);
 }
